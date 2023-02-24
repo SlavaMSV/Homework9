1e9000e26de516a561b5b94e72caf748b5b97322
SlavaMSV <viacheslav.maslov@gmail.com>
2023-02-24 07:01:09 +0300

    добавлена программа вывода чисел от N до 1 с помощью рекурсии

---
+++ ex1/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Задайте значение N. 
+Напишите программу, которая выведет все натуральные числа 
+в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+int GetNumber()
+{
+
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine("Введите целое положительное число:");
+        if (int.TryParse(Console.ReadLine(), out result) && result >= 1)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод");
+        }
+    }
+    return result;
+}
+
+void WriteNumbers(int number)
+{
+    if (number == 1)
+    {
+        Console.Write("1");
+    }
+    else
+    {
+    Console.Write($"{number}  ");
+    WriteNumbers(number - 1);
+    }
+}
+
+int number = GetNumber();
+WriteNumbers(number);
+
